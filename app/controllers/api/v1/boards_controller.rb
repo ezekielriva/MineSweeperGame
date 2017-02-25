1758918a653958ef1b3c5f23ba9da519d5f72629
Ezekiel <ezerivadeneiral@gmail.com>
2017-02-25 17:38:23 -0300

    Improve board api doc

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -1,10 +1,23 @@
 module Api
   module V1
     class BoardsController < BaseController
+      swagger_controller :boards, 'Board'
+
+      swagger_api :index do
+        summary 'List all boards'
+        response :ok, 'Success'
+      end
       def index
         render json: Board.all
       end
 
+      swagger_api :create do
+        summary 'Creates a board'
+        param :form, 'board[size_x]', :integer, :required, 'Board Size X'
+        param :form, 'board[size_y]', :integer, :required, 'Board Size Y'
+        param :form, 'board[no_mines]', :integer, :required, 'Number of Mines'
+        response :ok, 'Success', :Board
+      end
       def create
         Board.transaction do
           @board = Board.create(board_params)
@@ -19,6 +32,14 @@ module Api
 
       def board_params
         params.require(:board).permit(:size_x, :size_y, :no_mines)
+      end
+
+      swagger_model :Board do
+        description "A Board object."
+        property :id, :integer, :required
+        property :size_x, :integer, :required
+        property :size_y, :integer, :required
+        property :no_mines, :integer, :required
       end
     end
   end
